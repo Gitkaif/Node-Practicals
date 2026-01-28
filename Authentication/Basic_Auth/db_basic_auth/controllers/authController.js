ea65cb46bc7701ce2ac5e15bfe44c900f4faddcf
Gitkaifs <kaif.shaikh@b2winfotech.ai>
2026-01-28 10:53:06 +0530

    authentication folder

---
+++ Authentication/Basic_Auth/db_basic_auth/controllers/authController.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const model = require('../models/userModel')
+
+const addUser = async(req,res)=>{
+    const user = new model(req.body)
+    const result = await user.save()
+    res.status(201).json({
+        message:'user created...',result
+    })
+    
+}
+
+module.exports = addUser;
